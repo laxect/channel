4b1609fcd568c5549d8f11906ff3c2750f2b832b
gyara <me@gyara.moe>
2021-11-28 15:53:57 +0900

    add ss10

---
+++ pkgs/fonts.scm
@@ -0,0 +1,13 @@
+(define-public font-iosevka-term-ss10
+  (package
+    (inherit font-iosevka-term)
+    (name "font-iosevka-term")
+    (version (package-version font-iosevka))
+    (source
+     (origin
+       (method url-fetch/zipbomb)
+       (uri (string-append "https://github.com/be5invis/Iosevka"
+                           "/releases/download/v" version
+                           "/ttf-iosevka-term-ss10-" version ".zip"))
+       (sha256
+        (base32 "1asj7ah4w0sqz5kp1rf74ymrx53xhq5iv5xf9yrk14cnlinb1x38"))))))
